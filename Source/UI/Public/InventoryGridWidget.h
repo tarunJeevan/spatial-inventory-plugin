25b924b3c2f20920b4a00143bacc9bcd6a55a552
Tarun Jeevan <tjeevan200@gmail.com>
2025-08-28 21:55:44 -0500

    Create plugin structure and add required files

---
+++ Source/UI/Public/InventoryGridWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryGridWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_API UInventoryGridWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
---
+++ Source/UI/Public/ItemWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_API UItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
